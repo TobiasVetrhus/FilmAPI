f7e90b419fd3702f9512ec5e54eadf4d43a8c412
Ine Mari Bredesen <inemari.bredesen@no.experis.com>
2023-09-25 14:19:36 +0200

    Added exeptions

---
+++ Data/Exceptions/CharacterNotFound.cs
@@ -0,0 +1,13 @@
+﻿namespace FilmAPI.Data.Exceptions
+{
+    public class CharacterNotFound : EntityNotFoundException
+    {
+        public CharacterNotFoundException(int id)
+            : base("Character", id)
+        { }
+
+        public CharacterNotFoundException(string name)
+            : base("Character", name)
+        { }
+    }
+}
